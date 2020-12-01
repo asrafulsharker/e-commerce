f3271c33de9b18d4d6be98b363c9038000dade8a
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-02 00:12:35 +0600

    add gif

---
+++ src/components/Cover.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import CoverImg from './kng-cover.gif';
+export default function Cover() {
+    return (
+        <div>
+            <img src={CoverImg} style={{width:"100%",height:"450px"}}alt="cover"/>
+        </div>
+    )
+}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import Product from './Product';
 import Title from './Title';
 import {ProductConsumer, ProductProvider} from '../context';
+import Cover from './Cover';
 export default class ProductList extends Component {
  
     render() {
         return (
             <React.Fragment>
+                <Cover/>
                 <dic className="py-5">
                     <div className="container">
                         <Title name="our" title="products"/>
