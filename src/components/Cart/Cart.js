dbb46134d5b3d2ffdbf2b84e271cbd054084592c
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-01 23:04:05 +0600

    add cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Title from '../Title';
+import CartColumn from './CartColumns';
+import EmptyCart from './EmptyCart';
+import {ProductConsumer } from '../../context';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
+export default class  extends Component {
+    render() {
+        return (
+           <section>
+               <ProductConsumer>
+                   {value=>{
+                       const { cart} = value;
+                       if(cart.length >0){
+                           return(
+                               <React.Fragment>
+                                    <Title name="your" title="cart"/>
+                                    <CartColumn/>
+                                    <CartList value={value}/>
+                                    <CartTotals value={value}></CartTotals>
+                               </React.Fragment>
+                           );
+                       }
+                       else{
+                        return <EmptyCart />;
+                       }
+                   }}
+               </ProductConsumer>
+               
+           </section>
+        )
+    }
+}
--- src/context.js
+++ src/context.js
@@ -12,6 +12,9 @@ class ProductProvider extends Component {
         cart:[],
         modalOpen:false,
         modalProduct:detailProduct,
+        cartSubTotal:0,
+        cartTax:0,
+        cartTotal:0,
     }
     componentDidMount(){
         this.setProducts();
@@ -49,7 +52,7 @@ class ProductProvider extends Component {
         this.setState(()=>{
             return { product: tempProducts,cart:[...this.state.cart,product]};
         },()=>{
-            console.log(this.state);
+            this.addTotals();
         });
     };
     openModal =id =>{
@@ -62,6 +65,70 @@ class ProductProvider extends Component {
 this.setState(()=>{
     return {modalOpen:false};
 })
+   };
+   increment = (id) =>{
+       let tempCart = [...this.state.cart];
+       const selectedProduct = tempCart.find(item=>item.id === id)
+       const index =  tempCart.indexOf(selectedProduct);
+       const product = tempCart[index];
+       product.count = product.count + 1;
+       product.total = product.count * product.price;
+
+       this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+   };
+   decrement = (id) =>{
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item=>item.id === id)
+    const index =  tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count -1;
+    if(product.count === 0){
+        this.removeItem(id);
+    }else{
+        product.total = product.count * product.price;
+        this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+    }
+   }
+   removeItem=(id)=>{
+      let tempProducts = [...this.state.products];
+      let tempCart = [...this.state.cart];
+      tempCart = tempCart.filter(item => item.id !== id);
+      const index = tempProducts.indexOf(this.getItem(id));
+      let removedProduct = tempProducts[index];
+      removedProduct.inCart = false;
+      removedProduct.count = 0;
+      removedProduct.total = 0;
+this.setState(()=>{
+    return{
+        cart:[...tempCart],
+        products:[...tempProducts]
+    }
+},()=>{
+    this.addTotals();
+})
+   }
+   clearCart=()=>{
+       this.setState(()=>{
+           return{cart:[]};
+       },()=>{
+           this.setProducts();
+           this.addTotals();
+       })
+   }
+   addTotals=()=>{
+       let subTotal=0;
+       this.state.cart.map(item =>(subTotal += item.total));
+       const tempTax = subTotal *0.1;
+       const tax = parseFloat(tempTax.toFixed(2));
+       const total = subTotal + tax;
+       this.setState(()=>{
+           return{
+               cartSubTotal:subTotal,
+               cartTax:tax,
+               cartTotal:total
+           }
+       })
    }
     render() {
         return (
@@ -71,6 +138,10 @@ this.setState(()=>{
                 addToCart:this.addToCart,
                 openModal:this.openModal,
                 closeModal:this.closeModal,
+                increment:this.increment,
+                decrement:this.decrement,
+                removeItem:this.removeItem,
+                clearCart:this.clearCart,
             }}>
                 {this.props.children}
             </ProductContext.Provider>
