c00b1a79a0b6faaf8bbf5093d6a5665726f2279e
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-12-01 11:19:51 +0600

    update productjs

--- src/context.js
+++ src/context.js
@@ -7,8 +7,21 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products:storeProducts,
+        products:[],
         detailProduct:detailProduct
+    }
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts=()=>{
+        let tempProducts = [];
+        storeProducts.forEach(item=>{
+            const singleItem = {...item};
+            tempProducts=[...tempProducts,singleItem];
+        })
+        this.setState(()=>{
+            return{products:tempProducts};
+        })
     }
     handleDetail=()=>{
         console.log('Hello from detail');
@@ -16,6 +29,7 @@ class ProductProvider extends Component {
     addToCart=()=>{
         console.log('Hello from add to cart');
     }
+   
     render() {
         return (
             <ProductContext.Provider value={{
